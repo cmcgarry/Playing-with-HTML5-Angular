deb414a92e49dbce6c377ec552894ff8aca08c50
Collin McGarry <collinmcgarry@gmail.com>
2015-02-21 14:56:20 -0500

    basic framework

---
+++ pub/js/app.js
@@ -0,0 +1,12 @@
+/* All JavaScript code MUST be declared inside a closure wrapping the whole file and
+this closure MUST be in strict mode.*/
+(function () {
+  "use strict";
+
+  // All the JavaScript for this file.
+  var app = angular.module('paths', [
+      'ui.router',
+      'ui.bootstrap'
+  ]);
+
+})();
